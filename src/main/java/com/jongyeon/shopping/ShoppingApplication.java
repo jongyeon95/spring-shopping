c1dfc44003c42279f3cd2e5e84760cacd371c59c
whddus9509 <whddus9509@naver.com>
2020-08-04 15:28:22 +0900

    new project

---
+++ src/main/java/com/jongyeon/shopping/ShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.jongyeon.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingApplication.class, args);
+    }
+
+}
